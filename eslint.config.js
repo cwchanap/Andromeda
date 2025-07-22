67492b842b572a333d507d73f1df9698c6ca0002
cwchanap <cwchanap@connect.ust.hk>
2025-07-22 09:20:22 -0700

    chore: update package.json to add linting and formatting scripts, and include necessary devDependencies

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginAstro from "eslint-plugin-astro";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+export default tseslint.config(
+  {
+    ignores: ["dist", ".astro"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginAstro.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "no-unused-vars": "warn"
+    },
+  },
+);
